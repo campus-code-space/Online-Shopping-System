c094b69936c7dea54ca2fc9e0d25241f24547d54
Yosef <yosefetene02@gmail.com>
2025-06-08 11:53:57 +0300

    Temporarly removed inotify to stop container from restarting when .php files change

---
+++ watch.sh
@@ -0,0 +1,34 @@
+echo "Starting development server..."
+
+# Install dependencies
+echo "Installing composer dependencies..."
+composer install
+
+# Run migration and start the server initially
+php artisan migrate
+php artisan serve --host=0.0.0.0 --port=8000 
+
+# # Monitor file changes inside the container
+# while inotifywait -r -e modify,create,delete --include "\.php$" /var/www/html; do
+#     echo "Files changed. Restarting Laravel development server..."
+
+#     # Get the PID of the process running on port 8000
+#     SERVER_PID=$(lsof -t -i:8000)
+
+#     if [ -n "$SERVER_PID" ]; then
+#         echo "Stopping server with PID $SERVER_PID..."
+#         kill -9 $SERVER_PID
+#     else
+#         echo "No process running on port 8000."
+#     fi
+
+#     # Give some time for the process to terminate
+#     sleep 3
+
+#     # Run migrations and restart the server
+#     php artisan cache:clear
+#     php artisan config:clear
+#     php artisan route:clear    
+#     php artisan migrate
+#     php artisan serve --host=0.0.0.0 --port=8000 &
+# done
